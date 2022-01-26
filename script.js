5b5b5d747ea3ce2e7c24b21de2c1ea6471b806d4
Guilherme Barros <barros.guilherme21@gmail.com>
2022-01-26 08:53:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-/* JavaScript DOM Exercises 01 */
-
-/*
-  Exercise 01
-  -----------
-  Highlight all of the words over 8 characters long in the paragraph text (with a yellow background for example)
-*/
 const paragraph = document.querySelector('p');
 paragraph.innerHTML = paragraph.innerText
 .split(' ')
 .map(word => word.length > 8 ? `<span style="background-color : yellow">${word}</span>` :word )
 .join(' ');
 
-/*
-  Exercise 02
-  -----------
-  Add a link back to the source of the text after the paragraph tag.
-  (http://officeipsum.com/)
-*/
+
 const link = document.createElement('a');
 link.href = 'http://officeipsum.com/';
 link.innerText = "Link is here";
@@ -27,7 +15,7 @@ document.body.appendChild(link);
   Split each new sentence on to a separate line in the paragraph text.
   A sentence can be assumed to be a string of text terminated with a period (.)
 */
-paragraph.innerHTML = paragraph.innerHTML.split('.').join('.</p><p>' + '</p>');
+paragraph.innerHTML = paragraph.innerHTML.split('.').join('.</p><p>') + '</p>';
 
 /* 
   Exercise 04
@@ -35,7 +23,7 @@ paragraph.innerHTML = paragraph.innerHTML.split('.').join('.</p><p>' + '</p>');
   Count the number of words in the paragraph tag and display the count afer the heading.
   You can assume that all words are separated by one singular whitespace.
 */
-const wordCount = paragraph.innerText.split(' ').length;
+const wordCount = paragraph.innerText.trim().split(' ').length;
 const wordCountElement = document.createElement('div');
 wordCountElement.innerText = `${wordCount} words in the text`;
 document.body.insertBefore(wordCountElement,paragraph);
